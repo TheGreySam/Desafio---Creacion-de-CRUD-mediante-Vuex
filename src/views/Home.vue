c1cdb84d96541d592c1e5835b92b9719130e5a60
Sebastián Gómez Letelier <sgl1994@gmail.com>
2021-09-30 21:35:46 -0300

    III commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,46 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+
+    <ul>
+      <li v-for="paciente in pacientes" :key="paciente.id">
+        {{ paciente.nombre }} {{ paciente.apellido }} - {{ paciente.edad }} |
+         {{ paciente. }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import Firebase from "firebase"
 import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
+  },
+  data: () => ({
+    pacientes: []
+  }),
+  mounted() {
+  //  Firebase.firestore()
+  //  .collection("pacientes")
+  //  .get()
+  //  .then(collection => {
+  //    collection.forEach((document) => {
+  //      this.pacientes.push({ id: document.id, ...document.data() });
+  //    });
+  //  });
+    Firebase.firestore()
+    .collection("pacientes")
+    .onSnapshot((querySnapshot) => {
+      this.pacientes = [];
+      querySnapshot.forEach(document => {
+        this.pacientes.push({ id: document.id, ...document.data() });
+
+      })
+    })
   },
 };
 </script>
