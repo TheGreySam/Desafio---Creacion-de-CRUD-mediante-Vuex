82c6b5662e75d9928d263db0af3157842a7b0663
Sebastián Gómez Letelier <sgl1994@gmail.com>
2021-10-03 20:45:34 -0300

    V commit

--- src/main.js
+++ src/main.js
@@ -1,13 +1,28 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-Vue.config.productionTip = false;
+import Element from 'element-ui'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/js/bootstrap.bundle.js";
+
+
+import Firebase from "firebase"
+import { firebaseConfig } from "../firebase-config"
+//import { Message } from "element-ui" 
+
+Firebase.initializeApp(firebaseConfig);
+
+
+Vue.config.productionTip = false
+Vue.use(Element)
+
+
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount("#app");
+  Firebase,
+  render: h => h(App)
+}).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,56 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Store from '../store';
+import Login from '../views/Login.vue'
+//import Firebase from "firebase"
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: Home,
+    meta: {
+      login: false,
+    }
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: '/about',
+    name: 'About',
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    meta: {
+      login: true
+    }
   },
   {
-    path: "/login",
-    name: "Login",
-    component: () =>
-      import("../views/Login.vue")
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    meta: {
+      login: false
+    }
   }
-];
+]
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
-  routes,
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  if(to.meta.login) {
+    if (Store.state.currentUser) {
+    next();
+  } else {
+    next("/login")
+  }
+} else {
+  next();
+}
+
 });
 
 export default router;
